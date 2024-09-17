3b641781d5ecb4768d33815e11e2be297cddfc7c
Wael Zantour <waelzantour1@gmail.com>
2024-09-17 17:04:17 +0200

    Add the main work page

---
+++ src/app/components/pages/work/main-work/main-work.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-work',
+  templateUrl: './main-work.component.html',
+  styleUrl: './main-work.component.scss'
+})
+export class MainWorkComponent {
+
+}
