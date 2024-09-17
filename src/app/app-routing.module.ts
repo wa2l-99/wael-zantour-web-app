2e6dfc3227c319b3f365a031002d60b7c2ed4496
Wael Zantour <waelzantour1@gmail.com>
2024-09-17 17:02:00 +0200

    add navbar routes to the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutMeComponent } from './components/pages/about-me/about-me.component';
+import { MainWorkComponent } from './components/pages/work/main-work/main-work.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'aboutMe', component: AboutMeComponent },
+  { path: 'myMainWork', component: MainWorkComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
