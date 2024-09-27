96ad9c128b1f2541bc889cdd14909883b9f864bb
Wael Zantour <waelzantour1@gmail.com>
2024-09-28 01:00:20 +0200

    Add popover for skills discription

--- src/app/components/skill-set/skill-set.component.ts
+++ src/app/components/skill-set/skill-set.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-skill-set',
   templateUrl: './skill-set.component.html',
-  styleUrl: './skill-set.component.scss'
+  styleUrl: './skill-set.component.scss',
 })
 export class SkillSetComponent {
+  constructor(private renderer: Renderer2) {}
 
+  changePopoverColor(color: string) {
+    setTimeout(() => {
+      const popoverElement = document.querySelector(`.popover-header`);
+      if (popoverElement) {
+        this.renderer.setStyle(popoverElement, 'color', color);
+      }
+    }, 5);
+  }
 }
