16b4d9bbd4928b6a9a15eef4d67464484b9f2e1f
Wael Zantour <waelzantour1@gmail.com>
2024-09-28 01:03:42 +0200

    add navaigation for the active section

--- src/app/components/pages/about-me/about-me.component.ts
+++ src/app/components/pages/about-me/about-me.component.ts
@@ -1,23 +1,53 @@
-import { AfterViewInit, Component, ElementRef, Renderer2 } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss',
+  styleUrls: ['./about-me.component.scss'],
 })
-export class AboutMeComponent implements AfterViewInit {
-  active = 1;
+export class AboutMeComponent implements OnInit {
+  activeTab: any = 0;
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  constructor(private route: ActivatedRoute) {}
 
-  ngAfterViewInit(): void {
-    const contents =
-      this.el.nativeElement.querySelectorAll('.animated-slide-up');
+  ngOnInit(): void {
+    // Abonnement aux changements de fragment dans l'URL
+    this.route.fragment.subscribe((fragment: string | null) => {
+      if (fragment) {
+        this.handleFragmentChange(fragment);
+        this.scrollToFragment(fragment);
+      }
+    });
+  }
 
+  // Fonction pour activer l'onglet basé sur le fragment
+  handleFragmentChange(fragment: string) {
+    switch (fragment) {
+      case 'education':
+        this.activeTab = 0;
+        break;
+      case 'experience':
+        this.activeTab = 1; // Activer "Expérience professionnelle"
+        break;
+      case 'skills':
+        this.activeTab = 2; // Activer "Compétences"
+        break;
+      default:
+        this.activeTab = 0; // Onglet par défaut si fragment inconnu
+    }
+  }
+
+  // Utiliser window.scrollTo pour forcer le défilement exact au début de la section
+  scrollToFragment(fragment: string) {
     setTimeout(() => {
-      contents.forEach((content: any) => {
-        this.renderer.addClass(content, 'visible');
-      });
-    }, 100);
+      const element = document.getElementById(fragment);
+      if (element) {
+        const yOffset = -150;
+        const yPosition =
+          element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+        window.scrollTo({ top: yPosition, behavior: 'smooth' });
+      }
+    }, 10); // Attendre 500ms pour s'assurer que le DOM est prêt
   }
 }
